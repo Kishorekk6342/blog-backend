1f08a7f7c68fca4928653b2f7c4c29f464a55f27
Kishorekk6342 <kishorejayabal10@gmail.com>
2026-02-23 18:11:50 +0530

    swageer

--- Program.cs
+++ Program.cs
@@ -112,7 +112,7 @@ var app = builder.Build();
 
 // -------------------- PIPELINE --------------------
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
